21e826b1a19d2922f4053f3a2b43b817a7efcf18
Clone <37975817+Femikims1980@users.noreply.github.com>
2018-09-18 15:28:40 -0400

    Cache and computing inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is expected to cache the inverse of matrix(rnorm(12),3,3,3))
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(rnorm(12),3,3,3)) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## Below computes the inverse of the matrix above
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x = matrix(rnorm(12),3,3,3)) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)){
+        message("computing cache")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setInverse(inv)
+    inv
 }
+
+### Validation 
+##mat <- matrix(rnorm(12),3,3,3)
+##mat1 <- makeCacheMatrix(mat)
+##cacheSolve(mat1)
+
+
+
+
